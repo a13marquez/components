0230bd33cbb9b0192fdf7336effd86b48ccb4968
Álvaro Márquez <marquezalvaro13@gmail.com>
2019-06-09 16:56:19 +0200

    Fix styledHeader

--- packages/react/components/Header/StyledHeader.js
+++ packages/react/components/Header/StyledHeader.js
@@ -5,7 +5,7 @@ const StyledHeader = styled.header`
   position: fixed;
   z-index: 10;
   top: 0;
-  left: 0;4
+  left: 0;
   width:  100%;
   height: ${props => props.height || '80px'};
   color: ${props => props.color || 'white' };
